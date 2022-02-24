5acd33c0aea87608468090422d9606ba0ce6707d
Ben <burlesonbenjamin@gmail.com>
2022-02-23 18:18:51 -0600

    react 2

--- assets/js/React.js
+++ assets/js/React.js
@@ -1,5 +1,10 @@
 class email22 extends React.Component {
-
-
+    constructor(props){
+        super(props);
+        this.state = {
+            name; ',
+                apppVersion'
+        }
+    }
 
 }
